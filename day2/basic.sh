eb29815be2da5d76dfbf0a0728036814259036e5
Shubhankar Bagchi <bshubhankarops@gmail.com>
2024-02-13 20:07:09 +0530

    simple script created

---
+++ day2/basic.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+<< comm
+this is 
+multi line
+commnet
+comm
+
+name="shubam22"
+echo "name is $name and date is $(date)"
+
+read username
+
+echo "you entered $username"
+
+echo "arguments is script are $0 $1 $2 "
